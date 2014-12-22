565cbd83146b1f00f26fd8b06694425e9a36c1f0
Piotr Praszmo <ppraszmo@gmail.com>
2014-12-22 15:58:30 +0100

    randomize diamond animation

--- src/com/github/banthar/upg5/Diamond.hx
+++ src/com/github/banthar/upg5/Diamond.hx
@@ -10,18 +10,29 @@ class Diamond extends Collectible {
 	
 	var image:Int;
 	
-	public function new(position) {
-		super(position);
-
-		this.animation = new Animation([
+	static var SHINE = [
 			 new Frame(0, 32),
 			 new Frame(1, 4),
 			 new Frame(2, 4),
 			 new Frame(3, 4),
-		]);
-	}
+		];
 
+	static var DULL = [
+			 new Frame(0, 0),
+		];
+
+		
+	public function new(position) {
+		super(position);
+		this.animation = new Animation();
+		this.animation.playNow(DULL);
+	}
+	
 	override function getFrame(board:Board) {
+		if (Math.random() < 0.01) {
+			animation.playNext(SHINE);
+			animation.playNext(DULL);
+		}
 		return animation.next();
 	}
 	
